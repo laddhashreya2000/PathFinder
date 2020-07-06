612f2a6e6b3cdb3432aab271d95ff665cf6d4845
Vinay Kumar Kotakonda <vikuko@microsoft.com>
2020-07-06 16:08:58 +0530

    Latest Mars Project changes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,56 @@
-var path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const extractSass = new ExtractTextPlugin('style.css');
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
-	entry: {
-		bundle: './src/index.js',
-	},
-  	output: {
-	    path: path.join(__dirname, 'dist'),
-	    filename: '[name].js',
-	    publicPath: 'dist/'
-  	},
-  	devtool: "source-map",
-	module: {
-		rules: [
-		    {test: /\.(jpe?g|png|gif|svg)$/, loader: 'file-loader?name=assets/[name].[ext]&publicPath=../dist/'},
-			{ 
-				test: /\.js$/, 
-				exclude: /node_modules/, 
-				loader: 'babel-loader' 
-			},
-			{
-	            test: /\.scss$/,
-	            loader: extractSass.extract({
-	                loader: [{
-		                loader: "css-loader", options: {
-		                    sourceMap: true
-		                }
-		            }, {
-		                loader: "sass-loader", options: {
-		                    sourceMap: true
-		                }
-		            }]
-	            })
-	        }
-		]
-	},
-	plugins: [
-        extractSass
+  entry:  {
+    pathfinding: './src/PathFinding.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          },
+        ],
+      },
     ]
+  },
+
+  devServer: {
+    contentBase: './dist',
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      "$":"jquery",
+      "jQuery":"jquery",
+      "window.jQuery":"jquery",
+      }),
+  ],
 };
