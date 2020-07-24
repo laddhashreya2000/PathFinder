ddd34f408f192a1df5578ce0553a97fec4ca398a
Shreya <2000sladdha@gmail.com>
2020-07-24 17:59:15 +0530

    JPF modified, only two conditions are used

--- src/finders/JumpPointFinder.js
+++ src/finders/JumpPointFinder.js
@@ -3,8 +3,6 @@
  */
 var DiagonalMovement = require('../core/DiagonalMovement');
 var JPFNeverMoveDiagonally = require('./JPFNeverMoveDiagonally');
-var JPFAlwaysMoveDiagonally = require('./JPFAlwaysMoveDiagonally');
-var JPFMoveDiagonallyIfNoObstacles = require('./JPFMoveDiagonallyIfNoObstacles');
 var JPFMoveDiagonallyIfAtMostOneObstacle = require('./JPFMoveDiagonallyIfAtMostOneObstacle');
 
 /**
@@ -19,10 +17,6 @@ function JumpPointFinder(opt) {
     opt = opt || {};
     if (opt.diagonalMovement === DiagonalMovement.Never) {
         return new JPFNeverMoveDiagonally(opt);
-    } else if (opt.diagonalMovement === DiagonalMovement.Always) {
-        return new JPFAlwaysMoveDiagonally(opt);
-    } else if (opt.diagonalMovement === DiagonalMovement.OnlyWhenNoObstacles) {
-        return new JPFMoveDiagonallyIfNoObstacles(opt);
     } else {
         return new JPFMoveDiagonallyIfAtMostOneObstacle(opt);
     }
