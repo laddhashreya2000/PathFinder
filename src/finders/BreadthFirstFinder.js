bb273696ced565d21df06eb3b4bc441286ea06e7
Mukta Wagle <muktai2001@gmail.com>
2020-07-24 11:36:16 +0530

    now this has to work

--- src/finders/BreadthFirstFinder.js
+++ src/finders/BreadthFirstFinder.js
@@ -2,15 +2,15 @@ var Util = require('../core/Util');
 var DiagonalMovement = require('../core/DiagonalMovement');
 
 /**
- * Breadth-First-Search path finder.
+ * Bi-directional Breadth-First-Search path finder.
  * @constructor
- * @param {Object} opt
+ * @param {object} opt
  * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
  *     Deprecated, use diagonalMovement instead.
  * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
  *     block corners. Deprecated, use diagonalMovement instead.
  * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
- * @param {boolean} opt.biDirectional whether bidirectional search or not.
+ * @param {boolean} opt.biDirectional if the search is both ways.
  */
 function BreadthFirstFinder(opt) {
     opt = opt || {};
@@ -32,59 +32,17 @@ function BreadthFirstFinder(opt) {
     }
 }
 
+
 /**
  * Find and return the the path.
  * @return {Array<Array<number>>} The path, including both start and
  *     end positions.
  */
 BreadthFirstFinder.prototype.findPath = function(startX, startY, endX, endY, grid) {
-    
-    bi = this.biDirectional;
 
-if(!bi){
-    var openList = [],
-        diagonalMovement = this.diagonalMovement,
-        startNode = grid.getNodeAt(startX, startY),
-        endNode = grid.getNodeAt(endX, endY),
-        neighbors, neighbor, node, i, l;
+    var bi = this.biDirectional;
 
-    // push the start pos into the queue
-    openList.push(startNode);
-    startNode.opened = true;
-
-    // while the queue is not empty
-    while (openList.length) {
-        // take the front node from the queue
-        node = openList.shift();
-        node.closed = true;
-
-        // reached the end position
-        if (node === endNode) {
-            return Util.backtrace(endNode);
-        }
-
-        neighbors = grid.getNeighbors(node, diagonalMovement);
-        for (i = 0, l = neighbors.length; i < l; ++i) {
-            neighbor = neighbors[i];
-
-            // skip this neighbor if it has been inspected before
-            if (neighbor.closed || neighbor.opened) {
-                continue;
-            }
-
-            openList.push(neighbor);
-            neighbor.opened = true;
-            neighbor.parent = node;
-        }
-    }
-    
-    // fail to find the path
-    return [];
-}
-
-
-else{
-  
+if(bi){
     var startNode = grid.getNodeAt(startX, startY),
         endNode = grid.getNodeAt(endX, endY),
         startOpenList = [], endOpenList = [],
@@ -158,12 +116,47 @@ else{
 
     // fail to find the path
     return [];
+}
+else{
+    var openList = [],
+        diagonalMovement = this.diagonalMovement,
+        startNode = grid.getNodeAt(startX, startY),
+        endNode = grid.getNodeAt(endX, endY),
+        neighbors, neighbor, node, i, l;
 
+    // push the start pos into the queue
+    openList.push(startNode);
+    startNode.opened = true;
+
+    // while the queue is not empty
+    while (openList.length) {
+        // take the front node from the queue
+        node = openList.shift();
+        node.closed = true;
+
+        // reached the end position
+        if (node === endNode) {
+            return Util.backtrace(endNode);
+        }
+
+        neighbors = grid.getNeighbors(node, diagonalMovement);
+        for (i = 0, l = neighbors.length; i < l; ++i) {
+            neighbor = neighbors[i];
+
+            // skip this neighbor if it has been inspected before
+            if (neighbor.closed || neighbor.opened) {
+                continue;
+            }
+
+            openList.push(neighbor);
+            neighbor.opened = true;
+            neighbor.parent = node;
+        }
+    }
+    
+    // fail to find the path
+    return [];
 }
-}
-;
+};
 
 module.exports = BreadthFirstFinder;
-
-
-
